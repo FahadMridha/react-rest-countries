6e32f6bd79bf7aa895de6a9fa30d2192417b8e20
fahadMridha <sohelcse16@gmail.com>
2022-09-23 01:02:14 +0600

    header and footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Countries from './components/Countries/Countries';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
+      <Header/>
      <Countries></Countries>
+     <Footer/>
     </div>
   );
 }
